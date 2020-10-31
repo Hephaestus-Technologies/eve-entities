e3dfeb418d57913ba04c5f75bc8437077238496f
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2020-10-31 05:45:55 -0600

    Compile output

---
+++ src/general/manufacturing-group.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+exports.ManufacturingGroup = void 0;
+var ManufacturingGroup;
+(function (ManufacturingGroup) {
+    ManufacturingGroup[ManufacturingGroup["CUSTOM"] = 0] = "CUSTOM";
+    ManufacturingGroup[ManufacturingGroup["BLUEPRINT"] = 1] = "BLUEPRINT";
+    ManufacturingGroup[ManufacturingGroup["REACTION"] = 2] = "REACTION";
+    ManufacturingGroup[ManufacturingGroup["PI"] = 3] = "PI";
+    ManufacturingGroup[ManufacturingGroup["MINERALS"] = 4] = "MINERALS";
+    ManufacturingGroup[ManufacturingGroup["ICE"] = 5] = "ICE";
+    ManufacturingGroup[ManufacturingGroup["SALVAGE"] = 6] = "SALVAGE";
+    ManufacturingGroup[ManufacturingGroup["DROP"] = 7] = "DROP";
+    ManufacturingGroup[ManufacturingGroup["MARKET_SPAWN"] = 8] = "MARKET_SPAWN";
+    ManufacturingGroup[ManufacturingGroup["OTHER"] = 9] = "OTHER";
+})(ManufacturingGroup = exports.ManufacturingGroup || (exports.ManufacturingGroup = {}));
---
+++ src/general/reaction-group.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.ReactionGroup = void 0;
+var ReactionGroup;
+(function (ReactionGroup) {
+    ReactionGroup[ReactionGroup["ADVANCED"] = 0] = "ADVANCED";
+    ReactionGroup[ReactionGroup["PROCESSED"] = 1] = "PROCESSED";
+    ReactionGroup[ReactionGroup["RAW"] = 2] = "RAW";
+    ReactionGroup[ReactionGroup["BOOSTER"] = 3] = "BOOSTER";
+    ReactionGroup[ReactionGroup["POLYMER"] = 4] = "POLYMER";
+})(ReactionGroup = exports.ReactionGroup || (exports.ReactionGroup = {}));
